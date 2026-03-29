d3d507724166474ed1234ca32fb6a11b47ecd05e
Ashwanth <tashwanth63@gmail.com>
2026-03-29 15:00:20 +0530

    feat : Added auth api endpoint

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -18,6 +18,7 @@ export const register = async ({ username, email, password }) => {
     console.error(error);
   }
 };
+
 export const login = async ({ email, password }) => {
   try {
     const res = await api.post("/api/auth/login", {
