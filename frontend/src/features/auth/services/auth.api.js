0ea59e419430b25c3b0e30cd5e0684e2ee2c1982
Ashwanth <tashwanth63@gmail.com>
2026-03-29 14:59:19 +0530

    feat : Added auth api endpoint

---
+++ frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5002",
+  withCredentials: true,
+});
+
+export const register = async ({ username, email, password }) => {
+  try {
+    const res = await api.post("/api/auth/register", {
+      username,
+      email,
+      password,
+    });
+
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const login = async ({ email, password }) => {
+  try {
+    const res = await api.post("/api/auth/login", {
+      email,
+      password,
+    });
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await api.get("/api/auth/logout");
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getMe = async () => {
+  try {
+    const res = await api.get("/api/auth/get-me");
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
